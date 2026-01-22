6d641b086b353eb7b50082d850c3e062b416c8a2
jossan-sj8 <sjoaquin545@gmail.com>
2026-01-22 16:54:43 -0300

    [Configuración] Creacion archivo empresa

---
+++ Project_Gon.Core/Entities/Empresa.cs
@@ -0,0 +1,14 @@
+﻿namespace Project_Gon.Core.Entities;
+
+public class Empresa
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = null!;
+    public string? Rut { get; set; }
+    public string? Region { get; set; }
+    public string? Ciudad { get; set; }
+    public string? Direccion { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool Activo { get; set; } = true;
+}
